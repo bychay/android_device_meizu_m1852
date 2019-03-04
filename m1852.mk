1076d6448c9da529770427af101cf2bc32576610
Wang Han <416810799@qq.com>
2019-03-04 19:15:19 +0800

    m1852: Remove com.android.future.usb.accessory from PRODUCT_PACKAGES
     * It is in core-minimal.mk.
    
    Change-Id: I2fff428fed504dedd0d3aa71ee80018a0880275e

--- m1852.mk
+++ m1852.mk
@@ -275,8 +275,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
-    com.android.future.usb.accessory
+    android.hardware.usb@1.0-service
 
 # VNDK-SP
 PRODUCT_PACKAGES += \
