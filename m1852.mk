d41e81e11cb5e4cc7e37fbeca3b93d38687c522b
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2020-06-01 12:01:27 +0530

    m1852: Remove deprecated android.hardware.thermal@1.0 HAL
    Remove deprecated android.hardware.thermal@1.0 HAL from
    device manifest.
    
    Bug: 154012150
    Change-Id: I884e6b96cac4ebc3abcfaf309388d526a30dc5bd

--- m1852.mk
+++ m1852.mk
@@ -250,12 +250,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm710
-
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.meizu_m1852
