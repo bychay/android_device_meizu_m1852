bec46ff9898c1435376f9abb09a60b537c0851a9
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    m1882: Build libhidltransport and libhwbinder

--- m1852.mk
+++ m1852.mk
@@ -158,7 +158,9 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    libhidltransport \
+    libhwbinder
 
 # Init
 PRODUCT_COPY_FILES += \
