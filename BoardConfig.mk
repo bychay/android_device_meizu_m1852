1936368b01338234813ff5f1a253e9e45ce8298a
Andrew <87061244+MisterZtr@users.noreply.github.com>
2021-09-15 14:58:59 +0300

    New VENDOR_SECURITY_PATCH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,7 +167,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-06-01
+VENDOR_SECURITY_PATCH := 2021-06-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
